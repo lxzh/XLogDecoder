e557a4556809fef4e0c6534b20a032ee99a112aa
Virogu <virogu@foxmail.com>
2023-09-15 16:04:23 +0800

    add flowWithTimeOut test

--- src/test/kotlin/common/coroutine/FlowTest.kt
+++ src/test/kotlin/common/coroutine/FlowTest.kt
@@ -5,6 +5,7 @@ import kotlinx.coroutines.flow.flow
 import kotlinx.coroutines.flow.launchIn
 import kotlinx.coroutines.flow.onEach
 import org.junit.jupiter.api.Test
+import kotlin.coroutines.resume
 
 @Suppress("DuplicatedCode")
 internal class FlowTest {
@@ -38,13 +39,55 @@ internal class FlowTest {
     fun flowLaunchInTest(): Unit = runBlocking {
         flow {
             repeat(10) {
-                kotlinx.coroutines.delay(100)
+                delay(100)
                 emit(it)
             }
         }.onEach {
             println(Thread.currentThread().name)
         }.launchIn(this)
         delay(10000)
+    }
+
+    @Test
+    fun flowWithTimeOut(): Unit = runBlocking {
+        val r = withContext(Dispatchers.IO) {
+            getSuspendCancellableCoroutineResult(this)
+        }
+        println("result: $r")
+    }
+
+    private suspend fun getSuspendCancellableCoroutineResult(
+        scope: CoroutineScope
+    ) = suspendCancellableCoroutine<Int?> { cont ->
+        fun resume(data: Int?) {
+            if (cont.isActive) {
+                cont.resume(data)
+            }
+        }
+
+        val f = flow {
+            repeat(10) {
+                delay(1000)
+                emit(it)
+            }
+        }
+
+        scope.launch {
+            try {
+                withTimeout(300){
+                    f.collect {
+                        println("collect it: $it")
+                        resume(it)
+                        //this@launch.cancel()
+                        cancel()
+                    }
+                }
+            } catch (e: Throwable) {
+                println(e)
+                resume(null)
+                //this.cancel()
+            }
+        }
     }
 
 }
