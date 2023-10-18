30dbc01ff1a5b3169f337f2dbc9d2ec405de790c
Virogu <virogu@foxmail.com>
2023-10-18 15:17:58 +0800

    update calculator demo

--- src/test/kotlin/demo/calculator/CalculatorTest.kt
+++ src/test/kotlin/demo/calculator/CalculatorTest.kt
@@ -6,66 +6,67 @@ import com.singularsys.jep.Jep
 import demo.calculator.tool.Calculator
 import kotlinx.coroutines.runBlocking
 import net.objecthunter.exp4j.ExpressionBuilder
+import net.sourceforge.jeval.Evaluator
+import org.cheffo.jeplite.JEP
+import org.cheffo.jeplite.util.DoubleStack
 import org.junit.jupiter.api.Test
 
 
 internal class CalculatorTest {
     @Test
     fun test(): Unit = runBlocking {
-        var s = "(((9*6)-3)/2)"
-        test1(s)
-        test2(s)
-        test3(s)
-        test4(s)
-        test5(s)
-        s = "7-6.3"
-        test1(s)
-        test2(s)
-        test3(s)
-        test4(s)
-        test5(s)
+        listOf(
+            "1.5E3*2",
+            "1.5*10^3*2",
+            "1.5/10^3*2",
+            "0*10^18*0",
+            "(((9-6)*3)/2)",
+            "7-6.3",
+        ).also(::testExpression)
     }
 
-    private fun test1(s: String) = runCatching {
-        Calculator.conversion(s).also {
-            println("test1: $s = $it")
+    private fun testExpression(list: List<String>) {
+        list.forEach { s ->
+            println("\ntest: $s")
+            functions.mapIndexed { index, function ->
+                val r = kotlin.runCatching {
+                    val r = function(s)
+                    println("test${index + 1}, $s = $r")
+                }.onFailure {
+                    println("test${index + 1}, error: $it")
+                }
+                "test${index + 1} success: ${r.isSuccess}"
+            }
         }
-    }.onFailure {
-        println("test1: error: $it")
     }
 
-    private fun test2(s: String) = runCatching {
-        ExpressionBuilder(s).build().evaluate().also {
-            println("test2: $s = $it")
+    private val functions = listOf<(String) -> Any>(
+        { s ->//1
+            Calculator.conversion(s)
+        },
+        { s ->//2
+            ExpressionBuilder(s).build().evaluate()
+        },
+        { s ->//3
+            Jep().let {
+                it.parse(s)
+                it.evaluate()
+            }
+        },
+        { s ->//4
+            Parser.parse(s).evaluate().toDouble()
+        },
+        { s ->//5
+            ExpressRunner().execute(s, null, null, true, false)
+        },
+        { s ->//6
+            Evaluator().evaluate(s)
+        },
+        { s ->//7
+            JEP().let {
+                it.parseExpression(s)
+                it.getValue(DoubleStack())
+            }
         }
-    }.onFailure {
-        println("test2: error: $it")
-    }
-
-    private fun test3(s: String) = runCatching {
-        Jep().let {
-            it.parse(s)
-            it.evaluate()
-        }.also {
-            println("test3: $s = $it")
-        }
-    }.onFailure {
-        println("test3: error: $it")
-    }
-
-    private fun test4(s: String) = runCatching {
-        Parser.parse(s).evaluate().toDouble().also {
-            println("test4: $s = $it")
-        }
-    }.onFailure {
-        println("test4: error: $it")
-    }
-
-    private fun test5(s: String) = runCatching {
-        ExpressRunner().execute(s, null, null, true, false).also {
-            println("test5: $s = $it")
-        }
-    }.onFailure {
-        println("test5: error: $it")
-    }
+    )
 }
