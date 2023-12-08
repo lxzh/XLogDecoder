e4ba9408f4893256d28f8edf6c44fb755face35f
Virogu <virogu@foxmail.com>
2023-12-08 15:32:29 +0800

    reformat

--- app/src/test/kotlin/ledou/URLExtension.kt
+++
@@ -1,5 +0,0 @@
-package ledou
-
-import java.net.URLDecoder
-
-val String.decoded: String get() = URLDecoder.decode(this, "UTF-8")
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,20 +1,18 @@
+// Copyright 2000-2022 JetBrains s.r.o. and contributors. Use of this source code is governed by the Apache 2.0 license.
 pluginManagement {
     repositories {
+        google()
         gradlePluginPortal()
-        google()
         mavenCentral()
+        maven("https://maven.pkg.jetbrains.space/public/p/compose/dev")
     }
-}
 
-dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
-    repositories {
-        mavenLocal()
-        google()
-        mavenCentral()
+    plugins {
+        kotlin("jvm").version("1.9.20")
+        kotlin("plugin.serialization").version("1.9.20")
+        id("org.jetbrains.compose").version("1.5.10")
     }
-}
 
-rootProject.name = "KTDemo"
-include(":leetCode")
-include(":app")
+}
+rootProject.name = "XLogDecoder"
+
